dc53c3830571f40f5cafc33670ca3ae872609cf0
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2024-10-30 22:27:56 +0530

    added NavigationContainer and Stack Navigator from react-navigation for navigation between different screens in App component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,68 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./src/screens/Home";
+import Details from "./src/screens/Details";
+import Settings from "./src/screens/Settings";
+import Login from "./src/screens/Login";
+import SignUp from "./src/screens/SignUp";
+import Main from "./src/screens/Main";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Main Screen"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen
+          name="Settings"
+          options={{ headerShown: false }}
+          component={Settings}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Main Screen"
+          options={{ headerShown: false }}
+          component={Main}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context"
 
 const Main = () => {
   return (
-    <View>
+    <SafeAreaView style={{flex:1}}>
       <Text>Main</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
